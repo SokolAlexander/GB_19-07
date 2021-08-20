3f67a62f66c3850af1b6ebf6daebb0faa83ab849
SokolAlexander <asokol1994@gmail.com>
2021-08-20 16:34:40 +0300

    lesson10

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { Router } from "./components/Router";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Parent } from "./components/Example";
 
 function App() {
   return (
